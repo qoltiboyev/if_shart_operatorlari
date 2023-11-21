61a5a23562a9431eacadb645f012e8f08fdb4fb2
qoltiboyev <qamariddinoltiboyevdev@gmail.com>
2023-11-21 16:02:48 +0500

    uchta sonning musbatini aniqlovchi dastur

---
+++ if4.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Uchta son berilgan.Shu sonlar orasidan nechta musbat son borligini aniqlovchi dastur tuzing.
+  int summ = 0;
+  int son = -10;
+  int son2 = 11;
+  int son3 = 8;
+
+  if (son > 0) {
+    summ++;
+  }
+  if (son2 > 0) {
+    summ++;
+  }
+  if (son3 > 0) {
+    summ++;
+  }
+  print(summ);
+}
