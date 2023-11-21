e82205f5192e3cbd1b51a07a564bf43485ae2e68
qoltiboyev <qamariddinoltiboyevdev@gmail.com>
2023-11-21 14:19:28 +0500

    Musbat sonni 1ga oshiruvchi manfiy sonni 2ga kamaytiruvchi dastur

---
+++ if2.dart
@@ -0,0 +1,11 @@
+void main() {
+// Butun son berinlga.Agar berilgan son musbat bolsa,1 ga oshsin,aks holda bolsa 2 ga kamaytirilsin.
+
+  int nummber = 20;
+  if (nummber >= 0) {
+    nummber++;
+  } else {
+    nummber -= 2;
+  }
+  print(nummber);
+}
