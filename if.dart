3d1cb8160d0c12f8c5be15f40575b9533adbda08
qoltiboyev <qamariddinoltiboyevdev@gmail.com>
2023-11-21 13:41:54 +0500

    musbat son

---
+++ if.dart
@@ -0,0 +1,17 @@
+void main() {
+  // N sonini musbat yoki manfiylikka tekshiring
+  //print(5>4); katta:  true
+  //print(5<5);kichik:   false
+  //print(5==5);tenglik tekshiriladi:  true
+  //print(5!=4);teng emas:  ture
+  //print(5>=4);katta yoki teng: ture
+  //print(5<=5);kichik yoki teng:ture
+
+  int n = -10;
+
+  if (n > 0) {
+    print('Musbat son');
+  } else {
+    print('Musbat son emas');
+  }
+}
